662080b010f0b6e7c4294b01c04e698223a0cb62
priyanka garg <priyaanka.garg@gmail.com>
2023-12-12 11:49:32 -0500

    first form and prettier

---
+++ src/components/ContactForm.js
@@ -0,0 +1,59 @@
+// components/ContactForm.js
+
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+const ContactForm = ({ formData, onFormChange, onFormSubmit }) => {
+  return (
+    <form onSubmit={onFormSubmit}>
+      P
+      <input
+        type="text"
+        name="fullName"
+        required
+        placeholder="Enter a name..."
+        onChange={onFormChange}
+        value={formData.fullName}
+      />
+      <input
+        type="text"
+        name="address"
+        required
+        placeholder="Enter an address..."
+        onChange={onFormChange}
+        value={formData.address}
+      />
+      <input
+        type="text"
+        name="phoneNumber"
+        required
+        placeholder="Enter a phone number..."
+        onChange={onFormChange}
+        value={formData.phoneNumber}
+      />
+      <input
+        type="email"
+        name="email"
+        required
+        placeholder="Enter an email..."
+        onChange={onFormChange}
+        value={formData.email}
+      />
+      <button type="submit">Add</button>
+    </form>
+  )
+}
+
+ContactForm.propTypes = {
+  formData: PropTypes.shape({
+    fullName: PropTypes.string.isRequired,
+    address: PropTypes.string.isRequired,
+    phoneNumber: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+  }).isRequired,
+  onFormChange: PropTypes.func.isRequired,
+  onFormSubmit: PropTypes.func.isRequired,
+}
+
+export default ContactForm
--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,14 +1,43 @@
 // containers/AppContainer.js
+import React, { useState } from 'react';
 
-import React from 'react';
+import ContactForm from '../components/ContactForm';
 
 // Main AppContainer component for managing state and rendering components
 const AppContainer = () => {
-  
+  // State hooks for managing contacts and form data
+  const [formData, setFormData] = useState({
+    fullName: "",
+    address: "",
+    phoneNumber: "",
+    email: "",
+  })
+
+  // Handle form input change
+  const handleFormChange = (event) => {
+    const { name, value } = event.target
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }))
+  }
+
+  // Handle form submission (Add/Edit)
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+    setFormData({
+      fullName: "",
+      address: "",
+      phoneNumber: "",
+      email: "",
+    })
+  }
+
   // Return JSX for rendering the AppContainer component
   return (
     <div className="app-container">
-      HELLO
+      <ContactForm
+        formData={formData}
+        onFormChange={handleFormChange}
+        onFormSubmit={handleFormSubmit}
+      />
     </div>
   )
 }
