0f308005c87cd640d31020370ea726bdf60c3f41
priyanka garg <priyaanka.garg@gmail.com>
2023-12-12 11:27:32 -0500

    Initial commit - project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import AppContainer from './containers/AppContainer';
+
+// react App
+const App = () => {
+  return (
+    <div className="app-wrapper">
+      <AppContainer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/AppContainer.js
@@ -0,0 +1,16 @@
+// containers/AppContainer.js
+
+import React from 'react';
+
+// Main AppContainer component for managing state and rendering components
+const AppContainer = () => {
+  
+  // Return JSX for rendering the AppContainer component
+  return (
+    <div className="app-container">
+      HELLO
+    </div>
+  )
+}
+
+export default AppContainer
